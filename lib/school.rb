ae7a1212c1408a68441d843071b87fadc76adc27
Adam Workman <adammworkman25@gmail.com>
2020-06-21 23:19:03 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -2,12 +2,12 @@ class School
   
   attr_reader :name, :roster
   
-    
-
 def initialize(name)
   @name = name
   
-  @roster = {} #keys of grade levels... value of each key will be an arry of student names
+  @roster = {}
+  
+    #keys of grade levels... value of each key will be an arry of student names
    
     #   hash["new_key"] = []
     # hash["new_key"] << "new_value_for_value_array"
@@ -18,13 +18,23 @@ def initialize(name)
 end
   
   def add_student(student, grade) # student name and grade 
-  
+    self.roster[grade] ||= []
+    self.roster[grade] << student
   end
   
-  def grade(grade)  # should take in an argument of a grade and return all of the students in that grade
-  
+  def grade(grade) 
+    
+    # should take in an argument of a grade and return all of the students in that grade
+    
+    self.roster[grade]
   end
   
-  #You should be able to get a sorted list of all the students where the strings in the student arrays are sorted alphabetically
-  
+  def sort
+    
+    #You should be able to get a sorted list of all the students where the strings in the student array are sorted alphabetically
+    
+    self.roster.each do |grade, students|
+      students.sort!
+  end
+end
 end
