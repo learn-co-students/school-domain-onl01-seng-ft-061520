6649eb6ebdbc49c3352f75b050ff65a1fe67554e
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-23 22:04:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,18 +1,31 @@
 # code here!
 require 'pry'
+
 class School
   
-  attr_accessor :roster
+  attr_reader :roster, :name
   
   def initialize(name)
     @name = name
     @roster = {}
   end
   
-  def add_student(name, grade)
-    @add_student
+  def add_student(student_name, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = [student_name]
+    end 
   end  
-   
+
+  def grade(grade)
+    @roster[grade]
+  end
   
-  
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
 end 
