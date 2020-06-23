e154976f445058ae1c07a184ae774a34dea4a7fd
Amana Ammishaddai <amana.ammi@gmail.com>
2020-06-23 01:11:28 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+require 'pry'
+class School
+  
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @add_student
+  end  
+   
+  
+  
+end 
