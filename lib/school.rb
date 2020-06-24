68b29a254abad23ee7f68409656faa57c91f1ec5
Bryce Whittington <brycew30@gmail.com>
2020-06-24 17:29:41 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :roster, :name, :grade
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+     if roster.has_key?(grade)
+       roster[grade] << name
+     else
+       roster[grade] = [name]
+     end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    self.roster.each do |grade, students|
+      students.sort!
+    end
+  end
+end
