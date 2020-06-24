197d32801cac097f6a250ca5000c755abfedc7d1
kak79 <kohelka@gmail.com>
2020-06-23 20:12:13 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,15 +1,33 @@
+require 'pry'
 class School
-    attr_accessor :name, :roster
+    attr_accessor :name, :roster 
 
     def initialize(name)
         @name=name
         @roster = {}
     end
     
-    
+    def add_student(name,grade)
+        if roster.has_key?(grade)
+            roster[grade]<<name
+        else
+            roster[grade]=[name]
+        end
+   
+    end
+ 
+    def grade(grade)
+        #binding.pry
+        roster[grade]
+    end
 
-
-
-
+    def sort
+        sorted_roster={}
+        @roster.each do |grade,name|
+            #binding.pry
+            sorted_roster[grade]=name.sort
+        end
+        return sorted_roster
+    end
 
 end
